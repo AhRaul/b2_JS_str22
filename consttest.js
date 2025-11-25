8db1b0a6fcb1422a0b0222bda4916d7547db4461
RA <relian.skype@yandex.ru>
2025-11-25 14:49:00 +0300

    Загрузка кода

---
+++ consttest.js
@@ -0,0 +1,7 @@
+var pizza = true;
+pizza = false;
+console.log("var pizza = true \n now pizza = " + pizza); //false
+
+const cpizza = true;
+cpizza = false;
+console.log("const cpizza = true \n now cpizza = " + cpizza); //Error
